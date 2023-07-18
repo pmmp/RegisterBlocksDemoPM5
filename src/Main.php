34a39e48dda58228c710d44c54bcb9880928beaa
Maxime L <98211455+kaxyum@users.noreply.github.com>
2023-07-18 23:23:05 +0200

    Add ExtraVanillaItems and ExtraVanillaBlocks to the Creative Inventory

--- src/Main.php
+++ src/Main.php
@@ -9,8 +9,10 @@ use pocketmine\block\RuntimeBlockStateRegistry;
 use pocketmine\data\bedrock\block\BlockTypeNames;
 use pocketmine\data\bedrock\item\ItemTypeNames;
 use pocketmine\data\bedrock\item\SavedItemData;
+use pocketmine\inventory\CreativeInventory;
 use pocketmine\item\Item;
 use pocketmine\item\StringToItemParser;
+use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\world\format\io\GlobalBlockStateHandlers;
@@ -34,19 +36,33 @@ class Main extends PluginBase{
 
 	public static function registerBlocks() : void{
 		$block = ExtraVanillaBlocks::TARGET();
-		self::registerSimpleBlock(BlockTypeNames::TARGET, $block, ["target"]);
+		self::registerSimpleBlock(BlockTypeNames::TARGET, $block, ["target"], "all", "none");
 	}
 
 	public static function registerItems() : void{
 		$item = ExtraVanillaItems::IRON_HORSE_ARMOR();
-		self::registerSimpleItem(ItemTypeNames::IRON_HORSE_ARMOR, $item, ["iron_horse_armor"]);
+		self::registerSimpleItem(ItemTypeNames::IRON_HORSE_ARMOR, $item, ["iron_horse_armor"], "equipment", "none");
 	}
 
 	/**
 	 * @param string[] $stringToItemParserNames
 	 */
-	private static function registerSimpleBlock(string $id, Block $block, array $stringToItemParserNames) : void{
+	private static function registerSimpleBlock(string $id, Block $block, array $stringToItemParserNames, string $category, string $group) : void{
 		RuntimeBlockStateRegistry::getInstance()->register($block);
+
+        CreativeInventory::getInstance()->add($block->asItem());
+		CompoundTag::create()->setTag("minecraft:creative_category", CompoundTag::create()
+			->setString("category", $category)
+			->setString("group", $group));
+		CompoundTag::create()
+			->setTag("components",
+				CompoundTag::create()->setTag("minecraft:creative_category", CompoundTag::create()
+					->setString("category", $category)
+					->setString("group", $group)))
+			->setTag("menu_category", CompoundTag::create()
+				->setString("category", $category)
+				->setString("group", $group))
+			->setInt("molangVersion", 1);
 
 		GlobalBlockStateHandlers::getDeserializer()->mapSimple($id, fn() => clone $block);
 		GlobalBlockStateHandlers::getSerializer()->mapSimple($block, $id);
@@ -59,9 +75,23 @@ class Main extends PluginBase{
 	/**
 	 * @param string[] $stringToItemParserNames
 	 */
-	private static function registerSimpleItem(string $id, Item $item, array $stringToItemParserNames) : void{
+	private static function registerSimpleItem(string $id, Item $item, array $stringToItemParserNames, string $category, string $group) : void{
 		GlobalItemDataHandlers::getDeserializer()->map($id, fn() => clone $item);
 		GlobalItemDataHandlers::getSerializer()->map($item, fn() => new SavedItemData($id));
+
+        CreativeInventory::getInstance()->add($item);
+		CompoundTag::create()->setTag("minecraft:creative_category", CompoundTag::create()
+			->setString("category", $category)
+			->setString("group", $group));
+		CompoundTag::create()
+			->setTag("components",
+				CompoundTag::create()->setTag("minecraft:creative_category", CompoundTag::create()
+					->setString("category", $category)
+					->setString("group", $group)))
+			->setTag("menu_category", CompoundTag::create()
+				->setString("category", $category)
+				->setString("group", $group))
+			->setInt("molangVersion", 1);
 
 		foreach($stringToItemParserNames as $name){
 			StringToItemParser::getInstance()->register($name, fn() => clone $item);
